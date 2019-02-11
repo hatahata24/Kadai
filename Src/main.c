d3f3748809c4755ba580d65ef34c9ee07c96a4bf
Atsushi Hatanaka <walkman.ha@gmail.com>
2019-02-11 12:18:32 +0900

    上書きだよ

--- Src/main.c
+++ Src/main.c
@@ -110,6 +110,9 @@ int main(void)
   	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
   	  HAL_Delay(1000);
 
+  	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5, GPIO_PIN_RESET);
+  	  HAL_Delay(1000);
+
   	  /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
